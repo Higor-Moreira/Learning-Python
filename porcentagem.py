1773af691a99a67b81ec018bd92129308a5eabd7
Igor Oliveira <52582402+igoroliveira13@users.noreply.github.com>
2024-09-01 18:57:00 -0300

    Update porcentagem.py

--- porcentagem.py
+++ porcentagem.py
@@ -1,13 +1,20 @@
 """
 Como trabalhar com porcentagem usando Python
 
+1º conceito - 100% é sempre igual a 1, logo as porcentagens 
+
+30%, 50%, 80% correspodem a 0.3 0.5 e 0.8
+
+2º conceito - para saber qual o valor, basta multiplicar a porcentagem
+pelo valor. 
+
 """
 
-preco = 1000
+preco = 1000 # valor de um produto
 
-desconto = 0.25
+desconto = 0.25 # desconto da loja
 
-valor_desconto = preco * desconto
+valor_desconto = preco * desconto # para saber qual o valor do desconto
 
 print(preco, valor_desconto)
 
